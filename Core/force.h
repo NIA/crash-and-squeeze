5711534ca1de54dc176f3302371405923f337e98
NIA <nia.fti@mail.ru>
2010-08-18 16:52:46 +0700

    Force fixes

--- Core/force.h
+++ Core/force.h
@@ -10,13 +10,13 @@ namespace CrashAndSqueeze
         // * provides interface to check if it is applies to some point
         struct Force
         {
-            Math::Real magnitude;
+            Math::Vector value;
             bool is_active;
             
-            Force() : magnitude(0) {}
-            Force(Math::Real magnitude) : magnitude(magnitude) {}
+            Force() : is_active(true) {}
+            Force(Math::Vector value) : value(value), is_active(true) {}
             
-            virtual bool is_applied_to(Math::Vector const &point) = 0;
+            virtual bool is_applied_to(Math::Vector const &point) const = 0;
         };
 
         // A force applied pointwise
@@ -26,12 +26,12 @@ namespace CrashAndSqueeze
             Math::Real radius;
 
             PointForce() : radius(0) {}
-            PointForce(Math::Real magnitude, Math::Vector point_of_application, Math::Real radius)
-                :  Force(magnitude), point_of_application(point_of_application), radius(radius) {}
+            PointForce(Math::Vector value, Math::Vector point_of_application, Math::Real radius)
+                :  Force(value), point_of_application(point_of_application), radius(radius) {}
             
-            virtual /*override*/ bool is_applied_to(Vector const &point)
+            virtual /*override*/ bool is_applied_to(Math::Vector const &point) const
             {
-                return Math::less_or_equal( distance(point, point_of_application), radius )
+                return Math::less_or_equal( distance(point, point_of_application), radius );
             }
         };
 
@@ -43,23 +43,23 @@ namespace CrashAndSqueeze
             Math::Real max_distance;
 
             PlaneForce() : max_distance(0), plane_normal(1,0,0) {}
-            PlaneForce(Math::Real magnitude,
+            PlaneForce(Math::Vector value,
                        Math::Vector plane_point,
                        Math::Vector plane_normal,
                        Math::Real max_distance)
-                : Force(magnitude), plane_point(plane_point), plane_normal(plane_normal), max_distance(max_distance)
+                : Force(value), plane_point(plane_point), plane_normal(plane_normal), max_distance(max_distance)
             {
                 if(0 == plane_normal.sqared_norm())
-                    this->plane_normal = Vector(1,0,0);
+                    this->plane_normal = Math::Vector(1,0,0);
                 else
                     this->plane_normal /= this->plane_normal.norm();
 
             }
             
-            virtual /*override*/ bool is_applied_to(Vector const &point)
+            virtual /*override*/ bool is_applied_to(Math::Vector const &point) const
             {
                 return Math::less_or_equal( abs( (point - plane_point)*plane_normal ), max_distance );
             }
-        }
+        };
     }
 }
