80382d0167113a601046ff406fff6734fd376e59
NIA <nia.fti@mail.ru>
2010-09-08 18:01:52 +0700

    Force refactoring
    * extracted ForceWithRadius and Spring
    * no constructors and virtual methods in header

--- Core/force.cpp
+++ Core/force.cpp
@@ -4,11 +4,24 @@ namespace CrashAndSqueeze
 {
     using Math::Vector;
     using Math::Real;
+    using Math::less_or_equal;
     using Logging::logger;
 
     namespace Core
     {
         // -- abstract Force --
+        
+        Force::Force()
+        {
+            set_value(Vector::ZERO);
+            activate();
+        }
+
+        Force::Force(const Vector &value)
+        {
+            set_value(value);
+            activate();
+        }
 
         Vector Force::compute_value_at(const Vector &point, const Vector &velocity) const
         {
@@ -22,13 +35,41 @@ namespace CrashAndSqueeze
             return is_active() && is_applied_to(point) ? compute_value_at(point, velocity) : Vector::ZERO;
         }
 
-        // -- PointForce --
+        // -- EverywhereForce --
         
-        void PointForce::set_radius(Real radius)
+        EverywhereForce::EverywhereForce()
+        {
+        }
+        
+        EverywhereForce::EverywhereForce(const Vector &value)
+            : Force(value)
+        {
+        }
+        
+        bool EverywhereForce::is_applied_to(Vector const &point) const
+        {
+            ignore_unreferenced(point);
+            return true;
+        }
+        
+        // -- ForceWithRadius --
+        
+        ForceWithRadius::ForceWithRadius()
+        {
+            set_radius(0);
+        }
+
+        ForceWithRadius::ForceWithRadius(const Vector &value, Real radius)
+            : Force(value)
+        {
+            set_radius(radius);
+        }
+        
+        void ForceWithRadius::set_radius(Real radius)
         {
             if( radius < 0 )
             {
-                logger.warning("given radius for PointForce is less than 0, corrected to 0", __FILE__, __LINE__);
+                logger.warning("given radius for a subclass of ForceWidthRadius is less than 0, corrected to 0", __FILE__, __LINE__);
                 this->radius = 0;
             }
             else
@@ -37,7 +78,39 @@ namespace CrashAndSqueeze
             }
         }
 
+        // -- PointForce --
+
+        PointForce::PointForce()
+        {
+            set_point_of_application(Vector::ZERO);
+        }
+
+        PointForce::PointForce(const Vector &value, const Vector &point_of_application, Real radius)
+            :  ForceWithRadius(value, radius)
+        {
+            set_point_of_application(point_of_application);
+        }
+        
+        bool PointForce::is_applied_to(const Vector &point) const
+        {
+            return less_or_equal( distance(point, point_of_application), get_radius() );
+        }
+
         // -- PlaneForce --
+
+        PlaneForce::PlaneForce()
+        {
+            set_max_distance(0);
+        }
+
+        PlaneForce::PlaneForce(const Vector &value,
+                               const Vector &plane_point,
+                               const Vector &plane_normal,
+                               Real max_distance)
+            : Force(value), plane(plane_point, plane_normal)
+        {
+            set_max_distance(max_distance);
+        }
 
         void PlaneForce::set_max_distance(const Real &value)
         {
@@ -49,25 +122,75 @@ namespace CrashAndSqueeze
             }
         }
 
-        // -- HalfSpaceSpringForce --
-        void HalfSpaceSpringForce::set_spring_constant(Real value)
+        bool PlaneForce::is_applied_to(const Vector &point) const
+        {
+            return less_or_equal( plane.distance_to(point), max_distance );
+        }
+
+        // -- Spring --
+        
+        Spring::Spring()
+        {
+            set_spring_constant(0);
+            set_damping_constant(0);
+        }
+
+        Spring::Spring(Real spring_constant, Real damping_constant)
+        {
+            set_spring_constant(spring_constant);
+            set_damping_constant(damping_constant);
+        }
+
+        void Spring::set_spring_constant(Real value)
         {
             spring_constant = value;
             if(spring_constant < 0)
             {
                 spring_constant = 0;
-                logger.warning("given spring constant for HalfSpaceSpringForce is less than 0, corrected to 0", __FILE__, __LINE__);
+                logger.warning("given spring constant for Spring is less than 0, corrected to 0", __FILE__, __LINE__);
             }
         }
 
-        void HalfSpaceSpringForce::set_damping_constant(Real value)
+        void Spring::set_damping_constant(Real value)
         {
             damping_constant = value;
             if(damping_constant < 0)
             {
                 damping_constant = 0;
-                logger.warning("given damping constant for HalfSpaceSpringForce is less than 0, corrected to 0", __FILE__, __LINE__);
+                logger.warning("given damping constant for Spring is less than 0, corrected to 0", __FILE__, __LINE__);
             }
         }
+
+        Vector Spring::compute_force(const Vector &shift, const Vector &velocity) const
+        {
+            return - spring_constant*shift - damping_constant*velocity;
+        }
+
+        // -- HalfSpaceSpringForce --
+        
+        HalfSpaceSpringForce::HalfSpaceSpringForce()
+        {
+        }
+        
+        HalfSpaceSpringForce::HalfSpaceSpringForce(const Real spring_constant,
+                                                   const Vector &plane_point,
+                                                   const Vector &plane_normal,
+                                                   const Real damping_constant)
+            : spring(spring_constant, damping_constant),
+              plane(plane_point, plane_normal)
+        {
+        }
+        
+        bool HalfSpaceSpringForce::is_applied_to(const Vector &point) const
+        {
+            return plane.projection_to_normal(point) < 0;
+        }
+
+        Vector HalfSpaceSpringForce::compute_value_at(const Vector &point, const Vector &velocity) const
+        {
+            Real x = plane.projection_to_normal(point);
+            return spring.compute_force(x*plane.get_normal(), velocity);
+        }
+
     }
 }
--- Core/force.h
+++ Core/force.h
@@ -23,6 +23,9 @@ namespace CrashAndSqueeze
             virtual Math::Vector compute_value_at(const Math::Vector &point, const Math::Vector &velocity) const;
             
         public:
+            Force();
+            Force(const Math::Vector &value);
+            
             bool is_active() const { return is_active_; }
             void activate() { is_active_ = true; }
             void deactivate() { is_active_ = false; }
@@ -31,11 +34,7 @@ namespace CrashAndSqueeze
             void set_value(const Math::Vector &value) { this->value = value; }
             const Math::Vector & get_value() const { return value; }
 
-            Force() { activate(); set_value(Math::Vector::ZERO); }
-            Force(const Math::Vector &value) { set_value(value); activate(); }
-            
             virtual bool is_applied_to(const Math::Vector &point) const = 0;
-            
             Math::Vector get_value_at(const Math::Vector &point, const Math::Vector &velocity) const;
         };
 
@@ -43,41 +42,38 @@ namespace CrashAndSqueeze
         class EverywhereForce : public Force
         {
         public:
-            EverywhereForce() {}
-            EverywhereForce(const Math::Vector &value) : Force(value) {}
-            virtual /*override*/ bool is_applied_to(Math::Vector const &point) const
-            {
-                ignore_unreferenced(point);
-                return true;
-            }
+            EverywhereForce();
+            EverywhereForce(const Math::Vector &value);
+            virtual /*override*/ bool is_applied_to(Math::Vector const &point) const;
+        };
+
+        // An abstract force having property of radius
+        class ForceWithRadius : public Force
+        {
+        private:
+            Math::Real radius;
+        public:
+            ForceWithRadius();
+            ForceWithRadius(const Math::Vector &value, Math::Real radius);
+            
+            Math::Real get_radius() const { return radius; }
+            void set_radius(Math::Real radius);
         };
 
         // A force applied pointwise
-        class PointForce : public Force
+        class PointForce : public ForceWithRadius
         {
         private:
             Math::Vector point_of_application;
-            Math::Real radius;
 
         public:
+            PointForce();
+            PointForce(const Math::Vector &value, const Math::Vector &point_of_application, Math::Real radius);
+            
             const Math::Vector & get_point_of_application() const { return point_of_application; }
             void set_point_of_application(const Math::Vector &point) { point_of_application = point; }
 
-            Math::Real get_radius() const { return radius; }
-            void set_radius(Math::Real radius);
-
-            PointForce() { set_radius(0); }
-            PointForce(const Math::Vector &value, const Math::Vector &point_of_application, Math::Real radius)
-                :  Force(value)
-            {
-                set_point_of_application(point_of_application);
-                set_radius(radius);
-            }
-            
-            virtual /*override*/ bool is_applied_to(const Math::Vector &point) const
-            {
-                return Math::less_or_equal( distance(point, point_of_application), radius );
-            }
+            virtual /*override*/ bool is_applied_to(const Math::Vector &point) const;
         };
 
         // A force applied to all points on a plane
@@ -88,30 +84,42 @@ namespace CrashAndSqueeze
             Math::Real max_distance;
 
         public:
+            PlaneForce();
+            PlaneForce(const Math::Vector &value,
+                       const Math::Vector &plane_point,
+                       const Math::Vector &plane_normal,
+                       Math::Real max_distance);
+ 
             const Math::Vector & get_plane_point() const { return plane.get_point(); }
             const Math::Vector & get_plane_normal() const { return plane.get_normal(); }
             void set_plane(const Math::Vector &point, const Math::Vector &normal) { plane.set_point(point); plane.set_normal(normal); }
 
             Math::Real get_max_distance() const { return max_distance; }
             void set_max_distance(const Math::Real &value);
-
-            PlaneForce() { set_max_distance(0); }
-            PlaneForce(const Math::Vector &value,
-                       const Math::Vector &plane_point,
-                       const Math::Vector &plane_normal,
-                       Math::Real max_distance)
-                : Force(value)
-            {
-                set_max_distance(max_distance);
-                set_plane(plane_point, plane_normal);
-            }
             
-            virtual /*override*/ bool is_applied_to(const Math::Vector &point) const
-            {
-                return Math::less_or_equal( plane.distance_to(point), max_distance );
-            }
+            virtual /*override*/ bool is_applied_to(const Math::Vector &point) const;
         };
         
+        // An entity, used by all spring-like forces
+        class Spring
+        {
+        private:
+            Math::Real spring_constant;
+            Math::Real damping_constant;
+
+        public:
+            Spring();
+            Spring(Math::Real spring_constant, Math::Real damping_constant);
+            
+            Math::Real get_spring_constant() const { return spring_constant; }
+            void set_spring_constant(Math::Real value);
+
+            Math::Real get_damping_constant() const { return damping_constant; }
+            void set_damping_constant(Math::Real value);
+
+            Math::Vector compute_force(const Math::Vector &shift, const Math::Vector &velocity) const;
+        };
+
         // A force, pushing outside half-space, defined by plane_point and plane_normal.
         // A value is obtained by Hooke's law from position and spring_constant
         // "Outside" means positive half of an axis colinear to plane_normal.
@@ -119,40 +127,26 @@ namespace CrashAndSqueeze
         {
         private:
             Math::Plane plane;
-            Math::Real spring_constant;
-            Math::Real damping_constant;
+            Spring spring;
         public:
+            HalfSpaceSpringForce();
+            HalfSpaceSpringForce(const Math::Real spring_constant,
+                                 const Math::Vector &plane_point,
+                                 const Math::Vector &plane_normal,
+                                 const Math::Real damping_constant = 0);
+
             const Math::Vector & get_plane_point() const { return plane.get_point(); }
             const Math::Vector & get_plane_normal() const { return plane.get_normal(); }
             void set_plane(const Math::Vector &point, const Math::Vector &normal) { plane.set_point(point); plane.set_normal(normal); }
 
-            Math::Real get_spring_constant() const { return spring_constant; }
-            void set_spring_constant(Math::Real value);
+            Math::Real get_spring_constant() const { return spring.get_spring_constant(); }
+            void set_spring_constant(Math::Real value) { spring.set_spring_constant(value); }
 
-            Math::Real get_damping_constant() const { return damping_constant; }
-            void set_damping_constant(Math::Real value);
+            Math::Real get_damping_constant() const { return spring.get_damping_constant(); }
+            void set_damping_constant(Math::Real value) { spring.set_damping_constant(value); };
             
-            virtual /*override*/ bool is_applied_to(const Math::Vector &point) const
-            {
-                return plane.projection_to_normal(point) < 0;
-            }
-
-            virtual /*override*/ Math::Vector compute_value_at(const Math::Vector &point, const Math::Vector &velocity) const
-            {
-                Math::Real x = plane.projection_to_normal(point);
-                return - spring_constant*x*plane.get_normal() - damping_constant*velocity;
-            }
-
-            HalfSpaceSpringForce() { set_spring_constant(0); set_damping_constant(0); }
-            HalfSpaceSpringForce(const Math::Real spring_constant,
-                                 const Math::Vector &plane_point,
-                                 const Math::Vector &plane_normal,
-                                 const Math::Real damping_constant = 0)
-            {
-                set_spring_constant(spring_constant);
-                set_damping_constant(damping_constant);
-                set_plane(plane_point, plane_normal);
-            }
+            virtual /*override*/ bool is_applied_to(const Math::Vector &point) const;
+            virtual /*override*/ Math::Vector compute_value_at(const Math::Vector &point, const Math::Vector &velocity) const;
         };
     }
 }
