903daf4b6c25e618a6b65ea8a41d718975f465a9
NIA <nia.fti@mail.ru>
2011-03-16 16:57:24 +0600

    Moved single thread primitives from tests into library

---
+++ Parallel/single_thread_prim.h
@@ -0,0 +1,64 @@
+#pragma once
+#include "Parallel/iprim_factory.h"
+#include "Logging/logger.h"
+
+//
+// Defined here are synchronization primitives that doesn't do
+// any interthread synchronization and are supposed to be used
+// in a single-threaded application.
+//
+
+namespace CrashAndSqueeze
+{
+    namespace Parallel
+    {
+        // This implementation of ILock reports a failure if locked twice.
+        // Good for detecting deadlocks inside a single thread.
+        class SingleThreadLock : public ILock
+        {
+        private:
+            bool is_locked;
+        public:
+            SingleThreadLock() : is_locked(false) {}
+            virtual void lock()
+            {
+                if(is_locked)
+                    Logging::Logger::error("in SingleThreadLock::lock: unexpected attempt to lock already locked lock from the same thread!", __FILE__, __LINE__);
+                else
+                    is_locked = true;
+            }
+            virtual void unlock() { is_locked = false; }
+        };
+
+        // This implementation of IEvent reports a failure if wait()
+        // is called when the event not set, because in a single-threaded
+        // application it will be a "deadlock"
+        class SingleThreadEvent : public IEvent
+        {
+        private:
+            bool is_set;
+        public:
+            SingleThreadEvent(bool initially_set) : is_set(initially_set) {}
+            
+            virtual void set()   { is_set = true;  }
+            virtual void unset() { is_set = false; }
+
+            virtual void wait()
+            {
+                if( ! is_set )
+                    Logging::Logger::error("in SingleThreadEvent::wait: unexpected attempt to wait for unset event from the same thread!", __FILE__, __LINE__);
+            }
+        };
+
+        // A factory for these primitives which allocates them dynamically
+        class SingleThreadFactory : public IPrimFactory
+        {
+        public:
+            virtual ILock * create_lock() { return new SingleThreadLock(); }
+            virtual void destroy_lock(ILock * lock) { delete lock; }
+            
+            virtual IEvent * create_event(bool initially_set = false) { return new SingleThreadEvent(initially_set); }
+            virtual void destroy_event(IEvent * event) { delete event; }
+        };
+    }
+}
--- ToolsTester/task_queue_unittest.cpp
+++ ToolsTester/task_queue_unittest.cpp
@@ -1,5 +1,6 @@
 #include "tools_tester.h"
 #include "Parallel/task_queue.h"
+#include "Parallel/single_thread_prim.h"
 
 using namespace CrashAndSqueeze::Parallel;
 
@@ -9,57 +10,10 @@ protected:
     virtual void execute() { }
 };
 
-// This implementation of ILock makes the test fail
-// if it is locked again. Good for avoiding stupid deadlocks
-// inside a single thread
-class SingleThreadLock : public ILock
-{
-private:
-    bool is_locked;
-public:
-    SingleThreadLock() : is_locked(false) {}
-    virtual void lock()
-    {
-        ASSERT_FALSE(is_locked) << "Unexpected attempt to lock already locked lock from the same thread!";
-        is_locked = true;
-    }
-    virtual void unlock() { is_locked = false; }
-};
-
-// This implementation of IEvent makes the test fail
-// if wait() is called when the event not set, because
-// in a single-threaded application it will be a deadlock
-class SingleThreadEvent : public IEvent
-{
-private:
-    bool is_set;
-public:
-    SingleThreadEvent(bool initially_set) : is_set(initially_set) {}
-    
-    virtual void set()   { is_set = true;  }
-    virtual void unset() { is_set = false; }
-
-    virtual void wait()
-    {
-        ASSERT_TRUE(is_set) << "Unexpected attempt to wait for unset event from the same thread!";
-    }
-};
-
-// A factory for SingleThreadLock
-class STPrimFactory : public IPrimFactory
-{
-public:
-    virtual ILock * create_lock() { return new SingleThreadLock(); }
-    virtual void destroy_lock(ILock * lock) { delete lock; }
-    
-    virtual IEvent * create_event(bool initially_set = false) { return new SingleThreadEvent(initially_set); }
-    virtual void destroy_event(IEvent * event) { delete event; }
-};
-
 class TaskQueueTest : public ::testing::Test
 {
 protected:
-    STPrimFactory factory;
+    SingleThreadFactory factory;
     DummyTask t1, t2;
 
     static const int TQ_SIZE = 10;
